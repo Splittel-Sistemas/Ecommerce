fd514281ea20e0292d10e232630f31b1f0b0ae4f
aaron0609ac <58867893+aaron0609ac@users.noreply.github.com>
2020-08-18 18:46:11 -0500

    hotjar

--- views/Partials/Head.php
+++ views/Partials/Head.php
@@ -28,18 +28,18 @@
 <link rel="stylesheet" type="text/css" href="../../public/plugins/tingle/tingle.css">
 
 <!-- Hotjar Tracking Code for www.fibremex.com -->
-<!--
+
 <script>
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:1869227,hjsv:6};
+        h._hjSettings={hjid:1950599,hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 </script>
--->
+
 
 <?php 
 	@session_start();
