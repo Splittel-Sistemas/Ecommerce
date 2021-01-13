1dcd65252cab1ff8e0bdcc84ff32bbf105ad2d9c
Jesus Herrera <chucho_herrera7@hotmail.com>
2021-01-13 13:56:41 -0600

    correcciones

--- views/Pedido/3DSecure/Completado.php
+++ views/Pedido/3DSecure/Completado.php
@@ -1,20 +1,22 @@
-<?php $link = $_SESSION['Ecommerce-ClienteTipo'] == 'B2B' ? '../../Cuenta/index.php?menu=4' : '../../Cuenta/index.php?menu=3'; ?>
-<div class="card text-center">
-	<div class="card-body padding-top-2x">
-	<p><img src="../../public/images/img_spl/iconos/check.jpg" width="200px" height="200px" /></p>
-		<h3 class="card-title">¡Gracias por su orden!</h3>          
-		<p class="card-text">Su pedido se ha realizado y se procesará lo antes posible.</p>
-		<p class="card-text"> 
-			<u>Para ver los detalles de su compra:</u>
-		</p>
-		<div class="padding-top-1x padding-bottom-1x">
-			<a class="btn btn-outline-primary" onclick="ViewCuenta()"><i class="icon-user"></i>&nbsp;Ir a mi cuenta</a>
+<div class="container padding-bottom-3x mb-2">	
+	<?php $link = $_SESSION['Ecommerce-ClienteTipo'] == 'B2B' ? '../../Cuenta/index.php?menu=4' : '../../Cuenta/index.php?menu=3'; ?>
+	<div class="card text-center">
+		<div class="card-body padding-top-2x">
+		<p><img src="../../public/images/img_spl/iconos/check.jpg" width="200px" height="200px" /></p>
+			<h3 class="card-title">¡Gracias por su orden!</h3>          
+			<p class="card-text">Su pedido se ha realizado y se procesará lo antes posible.</p>
+			<p class="card-text"> 
+				<u>Para ver los detalles de su compra:</u>
+			</p>
+			<div class="padding-top-1x padding-bottom-1x">
+				<a class="btn btn-outline-primary" onclick="ViewCuenta()"><i class="icon-user"></i>&nbsp;Ir a mi cuenta</a>
+			</div>
 		</div>
 	</div>
 </div>
 
-<script>
-  var ViewCuenta = function(Elem){
-    window.parent.location.href = <?php echo $link ?>
-  } 
-</script>
+	<script>
+	var ViewCuenta = function(){
+		window.parent.location.href = <?php echo $link ?>
+	} 
+	</script>
--- views/Pedido/3DSecure/Incompleto.php
+++ views/Pedido/3DSecure/Incompleto.php
@@ -1,32 +1,34 @@
-<div class="card text-center">
-  <div class="card-body padding-top-2x">
-  <p><img src="../../public/images/img_spl/iconos/close.png" width="200px" height="200px" /></p>
-    <h3 class="card-title">&#33;Su orden no se ha generado!</h3>
-    <?php 
-      if (!class_exists("ErrorOpenPayController")) {
-        include $_SERVER['DOCUMENT_ROOT'].'/fibra-optica/models/Logs/ErrorOpenPay.Controller.php';
-      }
+<div class="container padding-bottom-3x mb-2">  
+  <div class="card text-center">
+    <div class="card-body padding-top-2x">
+    <p><img src="../../public/images/img_spl/iconos/close.png" width="200px" height="200px" /></p>
+      <h3 class="card-title">&#33;Su orden no se ha generado!</h3>
+      <?php 
+        if (!class_exists("ErrorOpenPayController")) {
+          include $_SERVER['DOCUMENT_ROOT'].'/fibra-optica/models/Logs/ErrorOpenPay.Controller.php';
+        }
 
-      $ErrorOpenPayController = new ErrorOpenPayController();
-      $ErrorOpenPayController->filter = "WHERE t16_f005 = ".$_SESSION['Ecommerce-PedidoKey']." ";
-      $ErrorOpenPayController->order = "ORDER BY t16_pk01 DESC LIMIT 1";
-      $ErrorOpenPay = $ErrorOpenPayController->GetBy();
+        $ErrorOpenPayController = new ErrorOpenPayController();
+        $ErrorOpenPayController->filter = "WHERE t16_f005 = ".$_SESSION['Ecommerce-PedidoKey']." ";
+        $ErrorOpenPayController->order = "ORDER BY t16_pk01 DESC LIMIT 1";
+        $ErrorOpenPay = $ErrorOpenPayController->GetBy();
 
-      if ($ErrorOpenPay) {
-      ?>
-    <p class="card-text"><?php echo $ErrorOpenPay->GetDescription(); ?></p>
-    <?php }else{ ?>
-    <p class="card-text">Vuelva a intentar de lo contrario favor de contactar a su ejecutivo de ventas.</p>
-    <?php } ?>
-    <p class="card-text"> 
-      <u>Para volver a iniciar su proceso de compra</u>
-    </p>
-    <div class="padding-top-1x padding-bottom-1x"><a class="btn btn-outline-primary" onclick="ViewCheckout()"><i class="icon-cart"></i>&nbsp;Reintentar</a></div>
+        if ($ErrorOpenPay) {
+        ?>
+      <p class="card-text"><?php echo $ErrorOpenPay->GetDescription(); ?></p>
+      <?php }else{ ?>
+      <p class="card-text">Vuelva a intentar de lo contrario favor de contactar a su ejecutivo de ventas.</p>
+      <?php } ?>
+      <p class="card-text"> 
+        <u>Para volver a iniciar su proceso de compra</u>
+      </p>
+      <div class="padding-top-1x padding-bottom-1x"><a class="btn btn-outline-primary" onclick="ViewCheckout()"><i class="icon-cart"></i>&nbsp;Reintentar</a></div>
+    </div>
   </div>
 </div>
 
 <script>
-  var ViewCheckout = function(Elem){
+  var ViewCheckout = function(){
     window.parent.location.href = "../../Checkout"
   } 
 </script>
