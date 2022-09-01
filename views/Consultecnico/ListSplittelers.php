f6ffb74ee4a5c517fd8ecfb4d738482770f3873f
Ramon Olea <50149082+Ramon-Olea@users.noreply.github.com>
2022-09-01 12:53:35 -0500

    Update ListSplittelers.php

--- views/Consultecnico/ListSplittelers.php
+++ views/Consultecnico/ListSplittelers.php
@@ -5,14 +5,14 @@
     }
     
     $ConsultecnicosController = new ConsultecnicosController();
-    $ConsultecnicosController->filter = "";
+    $ConsultecnicosController->filter = "WHERE activo='si'";
     $ResultConsultecnicos = $ConsultecnicosController->Get();
     foreach ($ResultConsultecnicos->records as $key => $Object_) {
 
   ?>
 
     <div class="col-sm-4 text-center mb-4"><img class="d-block w-150 mx-auto img-thumbnail rounded-circle mb-2" src="../../public/images/img_spl/splittellers/<?php echo $Object_->Imagen; ?>" alt="Team">
-        <h6 class="mb-1"><?php echo $Object_->Nombre.' '.$Object_->Apellido; ?></h6>
+        <h6 class="mb-1"><?php echo $Object_->Nombre.' sdsdsd'.$Object_->Apellido; ?></h6>
            <!-- <p class="text-sm text-muted mb-3">Founder, CEO</p>  -->
     </div>
 <?php
