e8327ad5977c855365277b5d707a74777db21181
Ramon Olea <50149082+Ramon-Olea@users.noreply.github.com>
2022-09-08 10:15:51 -0500

    cotizaciones

--- views/Cuenta/B2C/Cotizaciones/index.php
+++ views/Cuenta/B2C/Cotizaciones/index.php
@@ -6,7 +6,7 @@
       include $_SERVER['DOCUMENT_ROOT'].'/fibra-optica/models/Pedido/Pedido.Controller.php';
     }
     $PedidoController = new PedidoController();
-    $PedidoController->filter = "WHERE estatus = 'C' AND total > 0 AND fecha >= DATE_ADD(CURDATE(), INTERVAL -10 DAY) AND id_cliente = ".$_SESSION['Ecommerce-ClienteKey']." ";
+    $PedidoController->filter = "WHERE estatus = 'C' AND total > 0 AND fecha >= DATE_ADD(CURDATE(), INTERVAL -1 DAY) AND id_cliente = ".$_SESSION['Ecommerce-ClienteKey']." ";
     $PedidoController->order = "";
     $ResultPedido = $PedidoController->get();
 ?>
