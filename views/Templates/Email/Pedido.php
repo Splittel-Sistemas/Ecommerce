1def539bf077c128a9ce14bbf1d0af4bfd1807cf
Ramon Olea <50149082+Ramon-Olea@users.noreply.github.com>
2022-09-09 15:48:36 -0500

    prueba email

--- views/Templates/Email/Pedido.php
+++ views/Templates/Email/Pedido.php
@@ -119,44 +119,94 @@
 																unset($Pedido);
 																$html .= '</tbody>
 															</table>
-															<br>
-																						<div class="row padding-top-1x mt-3">
-							<div class="col-12 col-sm-3">
-								<h5>Datos de envió:</h5>
-								<ul class="list-unstyled">
-								<li><span class="text-muted">Cliente: </span>'. $_SESSION['Ecommerce-ClienteNombre'] .'</li>
-								<li><span class="text-muted">Dirección: </span> <span id="resumen-datosEnvio-direccion"></span></li>
-								<li><span class="text-muted">Teléfono: </span> <span id="resumen-datosEnvio-telefono"></span></li>
-								</ul>
-							</div>
+
+															<table role="presentation" border="0" cellpadding="0" cellspacing="0">
+															<tbody>
+																<tr style="width:100%;">
+																	<td style="margin-bottom: 10px; text-align: center; max-width:33%;"></td>
+																	<td style="margin-bottom: 10px; text-align: center; max-width:33%;"><img src="http://www.fibremex.com/ecomfmx/check.jpg" width="50" height="50"></td>
+																</tr>
+															</tbody>
+														</table><br>
+														<table role="presentation" border="0" cellpadding="0" cellspacing="0">
+															 <thead>
+																<tr style="width:100%;">
+																	<th style="margin-bottom: 20px; text-align: left; max-width:20%;">Datos de envió:</th>
+																	<th style="margin-bottom: 20px; text-align: left; max-width:50%;">Datos de Facturación:</th>
+																	<th style="margin-bottom: 20px; max-width:10%;">Paquetería</th>
+																	<th style="margin-bottom: 20px; max-width:10%;">Método de pago</th>
+																	<th style="margin-bottom: 20px; max-width:10%;">Moneda</th>
+																</tr>
+															</thead>
+															<tbody>';
+															/* $DetalleController = new DetalleController();
+															$Obj = $DetalleController->GetDetallePedido();
+
+															if($Obj->count > 0){
+																foreach ($Obj->records as $key => $data) {
+																	$detalleSubtotal = $data->PedidoMonedaPago == "USD" ? $data->DetalleSubtotal : $data->DetalleSubtotalMXN;
+																	$descripcion = !empty($data->ProductoDescripcion) ? $data->ProductoDescripcion : $data->ProductoConfigurableNombre;
+																	$html .= '<tr style="width:100%;">
+																		<td style="margin-bottom: 10px; text-align: left; max-width:20%;">'. $data->DetalleCodigo .'</td>
+																		<td style="margin-bottom: 10px; text-align: left; max-width:50%;">'. $descripcion .'</td>
+																		<td style="margin-bottom: 10px; text-align: center; max-width:10%;">'. $data->DetalleCantidad .'</td>
+																		<td style="margin-bottom: 10px; text-align: center; max-width:10%;"> $'.$detalleSubtotal .'</td>
+																		<td style="margin-bottom: 10px; text-align: center; max-width:10%;">'. $data->PedidoMonedaPago .'</td>
+																	</tr>';
+																}
+
+																$PedidoController = new PedidoController;
+																$PedidoController->filter = "WHERE id = ".$_SESSION["Ecommerce-PedidoKey"]." ";
+																$PedidoController->order = "";
+																# obtención de subtotal iva y total del pedido actual
+																$Pedido = $PedidoController->getBy();
+																
+																if($Pedido->MonedaPago == "USD"){
+																	$pedidoSubtotal = $Pedido->GetSubTotal();
+																	$pedidoIva = $Pedido->GetIva();
+																	$pedidoTotal = $Pedido->GetTotal(); 
+																}else{
+																	$pedidoSubtotal = $Pedido->GetSubTotalMXN();
+																	$pedidoIva = $Pedido->GetIvaMXN();
+																	$pedidoTotal = $Pedido->GetTotalMXN(); 
+																} */
 
 
-								<div class="col-12 col-sm-3">
-								<h5>Datos de Facturación:</h5>
-								<ul class="list-unstyled">
-									<li><span class="text-muted">Cliente: </span>'. $_SESSION['Ecommerce-ClienteNombre'] .'</li>
-									<li><span class="text-muted">Dirección: </span> <span id="resumen-datosFacturacion-direccion"></span></li>
-									<li><span class="text-muted">RFC: </span> <span id="resumen-datosFacturacion-RFC"></span></li>
-								</ul>
-								</div>
+														$html .= '<tr style="width:100%;">
+																				<td style="margin-bottom: 2px; text-align: center; max-width:20%;">Cliente: </span>'. $_SESSION['Ecommerce-ClienteNombre'] .'</td>
+																				<td style="margin-bottom: 2px; text-align: center; max-width:20%;">Dirección: </span>'. $_SESSION['Ecommerce-ClienteNombre'] .'</td>
+																				<td style="margin-bottom: 2px; text-align: center; max-width:20%;">Teléfono: </span>'. $_SESSION['Ecommerce-ClienteNombre'] .'</td>
+																				
 
-							<div class="col-12 col-sm-3">
-								<h5>Paquetería:</h5>
-								<ul class="list-unstyled">
-								<li id="resumen-paqueteria"></li>
-								</ul>
-							</div>
-							<div class="col-12 col-sm-3">
-								<h5>Método de pago:</h5>
-								<ul class="list-unstyled">
-								<li id="resumen-metodo-pago"></li>
-								</ul>
-								<h5>Moneda de pago:</h5>
-								<ul class="list-unstyled">
-								<li><span id="resumen-moneda-pago"></span></li>
-								</ul>
-							</div>
-							</div>
+																			</tr>
+																			<tr style="width:100%;">
+																			<td style="margin-bottom: 2px; text-align: center; max-width:20%;">Cliente: </span>'. $_SESSION['Ecommerce-ClienteNombre'] .'</td>
+																			<td style="margin-bottom: 2px; text-align: center; max-width:20%;">Dirección: </span>'. $_SESSION['Ecommerce-ClienteNombre'] .'</td>
+
+																				<td style="margin-bottom: 2px; text-align: center; max-width:20%;">RFC: </span>'. $_SESSION['Ecommerce-ClienteNombre'] .'</td>
+																			</tr>
+																			<tr style="width:100%;">
+																			<td style="margin-bottom: 2px; text-align: center; max-width:20%;">RFC: </span>'. $_SESSION['Ecommerce-ClienteNombre'] .'</td>
+																				
+																			</tr>
+																			<tr style="width:100%;">
+																			<td style="margin-bottom: 2px; text-align: center; max-width:20%;">RFC: </span>'. $_SESSION['Ecommerce-ClienteNombre'] .'</td>
+																				
+																			</tr>
+																			<tr style="width:100%;">
+																			<td style="margin-bottom: 2px; text-align: center; max-width:20%;">RFC: </span>'. $_SESSION['Ecommerce-ClienteNombre'] .'</td>
+																				
+																			</tr>';
+							 /*
+															}
+															 unset($DetalleController);
+															unset($Obj);
+															unset($PedidoController);
+															unset($Pedido); */
+															$html .= '</tbody>
+														</table>
+
+
 															<div style="text-align:right">
 															<div style="float: right">
 															</div>
