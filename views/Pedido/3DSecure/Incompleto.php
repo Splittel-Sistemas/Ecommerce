23d230876d66504fc813b2135da6d31e93312e8c
Ramon Olea <50149082+Ramon-Olea@users.noreply.github.com>
2022-09-20 11:50:15 -0500

    Update Incompleto.php

--- views/Pedido/3DSecure/Incompleto.php
+++ views/Pedido/3DSecure/Incompleto.php
@@ -36,7 +36,7 @@
           <p class="card-text"> 
             <u>Para volver a iniciar su proceso de compra</u>
           </p>
-          <div class="padding-top-1x padding-bottom-1x"><a class="btn btn-outline-primary" onclick="window.parent.location.href='../Login/logout.php'"><i class="icon-cart"></i>&nbsp;Reintentar</a></div>
+          <div class="padding-top-1x padding-bottom-1x"><a class="btn btn-outline-primary" onclick="window.parent.location.href='../Checkout'"><i class="icon-cart"></i>&nbsp;Reintentar</a></div>
         </div>
       </div>
     </div>
