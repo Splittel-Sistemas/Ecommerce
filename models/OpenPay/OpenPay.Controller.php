d0df6bf1af1cf96378606afe375150884c3c3afb
Ramon Olea <50149082+Ramon-Olea@users.noreply.github.com>
2022-09-21 12:18:29 -0500

    Update OpenPay.Controller.php

--- models/OpenPay/OpenPay.Controller.php
+++ models/OpenPay/OpenPay.Controller.php
@@ -49,7 +49,6 @@ class OpenPayController
     {
         try {
             if (!$this->Connection->conexion()->connect_error) {
-                var_dump($id);
                 $OpenPay_ =  new OpenPay_();
                 $OpenPay_->SetId($_SESSION['Ecommerce-OpenPayId']);
                 $OpenPay_->SetPublicKey($_SESSION['Ecommerce-OpenPayPrivateKey']);
