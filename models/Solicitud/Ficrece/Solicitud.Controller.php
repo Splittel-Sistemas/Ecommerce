83c3b3bd3ae0d5d6d220688058b916145dfd0a1e
Ramon Olea <50149082+Ramon-Olea@users.noreply.github.com>
2022-10-03 09:03:38 -0500

    prueba

--- models/Solicitud/Ficrece/Solicitud.Controller.php
+++ models/Solicitud/Ficrece/Solicitud.Controller.php
@@ -105,7 +105,7 @@ class SolicitudCController
         /* $ext = end(explode(".", $_FILES['file']['name']));	  */
   /*       $name1 = (hash('sha256', $_FILES['file']['name']) . '.' . $ext); */
         $ext1 = pathinfo( $_FILES['file']['name'],PATHINFO_EXTENSION );
-        $name1 =  ('1.' . $ext1);
+        $name1 =  ($_POST['Rfc']. '1.' . $ext1);
         if(!mkdir('../../../public/images/img_spl/ficrece/Archivos/'.$_POST['Rfc'].'/', 0777, true)) {
 
           die('Fallo al crear las carpetas...');
@@ -115,50 +115,50 @@ class SolicitudCController
         $SolicitudCModel->SetDoc1($name1);
 
         $ext2 = pathinfo( $_FILES['file2']['name'],PATHINFO_EXTENSION );
-        $name2 =  ('2.' . $ext2);
+        $name2 =  ($_POST['Rfc']. '2.' . $ext2);
         move_uploaded_file($_FILES['file2']['tmp_name'], '../../../public/images/img_spl/ficrece/Archivos/'.$_POST['Rfc'].'/' . $name2);
        # $file_nname2 = $_FILES['file2']['name'];
         $SolicitudCModel->SetDoc2($name2);
 
 
         $ext3 = pathinfo( $_FILES['file3']['name'],PATHINFO_EXTENSION );
-        $name3 =  ('3.' . $ext3);
+        $name3 =  ($_POST['Rfc']. '3.' . $ext3);
         move_uploaded_file($_FILES['file3']['tmp_name'], '../../../public/images/img_spl/ficrece/Archivos/'.$_POST['Rfc'].'/' .  $name3 );
         #$file_nname3 = $_FILES['file3']['name'];
         $SolicitudCModel->SetDoc3($name3);
 
         $ext4 = pathinfo( $_FILES['file4']['name'],PATHINFO_EXTENSION );
-        $name4 =  ('4.' . $ext4);
+        $name4 =  ($_POST['Rfc']. '4.' . $ext4);
         move_uploaded_file($_FILES['file4']['tmp_name'], '../../../public/images/img_spl/ficrece/Archivos/'.$_POST['Rfc'].'/' .  $name4);
         $file_nname4 = $_FILES['file4']['name'];
         $SolicitudCModel->SetDoc4($file_nname4);
 
         $ext5 = pathinfo( $_FILES['file5']['name'],PATHINFO_EXTENSION );
-        $name5 =  ('5.' . $ext5);
+        $name5 =  ($_POST['Rfc']. '5.' . $ext5);
         move_uploaded_file($_FILES['file5']['tmp_name'], '../../../public/images/img_spl/ficrece/Archivos/'.$_POST['Rfc'].'/' .  $name5);
         $file_nname5 = $_FILES['file5']['name'];
         $SolicitudCModel->SetDoc5($file_nname5);
 
         $ext6 = pathinfo( $_FILES['file6']['name'],PATHINFO_EXTENSION );
-        $name6 =  ('6.' . $ext6);
+        $name6 =  ($_POST['Rfc']. '6.' . $ext6);
         move_uploaded_file($_FILES['file6']['tmp_name'], '../../../public/images/img_spl/ficrece/Archivos/'.$_POST['Rfc'].'/' .  $name6);
         $file_nname6 = $_FILES['file6']['name'];
         $SolicitudCModel->SetDoc6($file_nname6);
 
         $ext7 = pathinfo( $_FILES['file7']['name'],PATHINFO_EXTENSION );
-        $name7 =  ('7.' . $ext7);
+        $name7 =  ($_POST['Rfc']. '7.' . $ext7);
         move_uploaded_file($_FILES['file7']['tmp_name'], '../../../public/images/img_spl/ficrece/Archivos/'.$_POST['Rfc'].'/' .  $name7);
         $file_nname7 = $_FILES['file7']['name'];
         $SolicitudCModel->SetDoc7($file_nname7);
 
         $ext8 = pathinfo( $_FILES['file8']['name'],PATHINFO_EXTENSION );
-        $name8 =  ('8.' . $ext8);
+        $name8 =  ($_POST['Rfc']. '8.' . $ext8);
         move_uploaded_file($_FILES['file8']['tmp_name'], '../../../public/images/img_spl/ficrece/Archivos/'.$_POST['Rfc'].'/' .  $name8);
         $file_nname8 = $_FILES['file8']['name'];
         $SolicitudCModel->SetDoc8($file_nname8);
 
         $ext9 = pathinfo( $_FILES['file9']['name'],PATHINFO_EXTENSION );
-        $name9 =  ('9.' . $ext9);
+        $name9 =  ($_POST['Rfc']. '9.' . $ext9);
         move_uploaded_file($_FILES['file9']['tmp_name'], '../../../public/images/img_spl/ficrece/Archivos/'.$_POST['Rfc'].'/' .  $name9);
         $file_nname9 = $_FILES['file9']['name'];
         $SolicitudCModel->SetDoc9($file_nname9);
