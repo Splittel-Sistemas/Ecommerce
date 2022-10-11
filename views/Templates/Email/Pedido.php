a4c9a6afdb00895bcc676b9ca2bfd67c46dd977a
Ramon Olea <50149082+Ramon-Olea@users.noreply.github.com>
2022-10-11 09:02:10 -0500

    Update Pedido.php

--- views/Templates/Email/Pedido.php
+++ views/Templates/Email/Pedido.php
@@ -289,14 +289,14 @@ class TemplatePedido
 						$html .= '';
 					};
 				} else {
-					if ($Pedido->DatosFacturacionKey != '') {
+				/* 	if ($Pedido->DatosFacturacionKey != '') {
 
 						foreach ($listGetBillToAdress as $key => $GetBillToAdress) {
 							$html .= '<td style="margin-bottom: 2px; text-align: left max-width:20%;">Dirección: ' . $GetBillToAdress->Street . ' No Ext. ' . $GetBillToAdress->StreetNo . ' Col. ' . $GetBillToAdress->Block . '</span></td>';
 						}
 					} else {
 						$html .= '';
-					};
+					}; */
 				};
 				$html .= '	
 														
@@ -317,7 +317,7 @@ class TemplatePedido
 					}
 				} else {
 
-					$html .= '';
+					$html .= '<td style="margin-bottom: 2px; text-align: left max-width:10%;"> </span></td>';
 				};
 
 
