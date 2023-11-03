e85e8a9760101273bf061a03417cb2a1bd2b81ba
aaron0609ac <58867893+aaron0609ac@users.noreply.github.com>
2023-11-03 15:03:28 -0600

    Se agrega patchcord CAT6 28AWG

--- public/scripts/Productos/Categorias/CableadoEstructurado/S000035.js
+++ public/scripts/Productos/Categorias/CableadoEstructurado/S000035.js
@@ -13,7 +13,7 @@ function validateEntero(valor) {
     document.getElementById('Longitud').value=valor.replace(/[^0-9]/g, '').replace(/(\..*)\./g, '$1')
   }
 }
-var Categoria6 = function(){
+var Categoria6UTP = function(){
   let Longitud = document.getElementById('Longitud')
   validateEntero(Longitud.value)
   let Color = document.getElementById('Color')
@@ -56,5 +56,65 @@ var Categoria6 = function(){
     agregarFichaTecnicaConfigurable(Marca+Familia+Estructurados.value)
     agregarCertificadoConfigurable(CodigoGenerado)
 }
+var Categoria628AWG = function(){
+  EstructuradosAlias='PCC6'
+  Categoria='CAT628AWG'
+  let Longitud = document.getElementById('Longitud')
+  validateEntero(Longitud.value)
+  let Color = document.getElementById('Color')
+  let LongitudIdText = document.getElementById('LongitudIdText')
+  let UnidadMedida = "P" // Pies
+  let Cubierta = "SEZH" // Pies
+  let CodigoGenerado = ""
+  let LongitudMin = 1
+  let LongitudMax = 25
+  StyleDisplayNoneOrBlock_2(Color, "none", [1, 2, 3, 4, 5])
+ // StyleDisplayNoneOrBlock_2(Color, 'none', [1,2])
+    LongitudIdText.innerHTML = 'Longitud (pies) '+LongitudMin+'~'+LongitudMax+':'
 
+    if (Longitud.value >= LongitudMin && Longitud.value <= LongitudMax) {
+      CodigoGenerado = Marca+Familia+EstructuradosAlias+NumeroConCeros2(Longitud.value, 2)+UnidadMedida+Color.value+Cubierta
+      let data =  { 
+        Action: 'calcular',
+        ActionCalcularPrecioPatchCord: true,
+        Longitud: Longitud.value,
+        Categoria: Categoria,
+        SubcategoriaN1Code: document.getElementById("CodeConfigurable").value
+      } 
+      CalcularPrecioPatchCords("../../models/Productos/PatchCord/CalcularPrecioPatchCord.Route.php", data)
+    }
+    // Agregación de codigo para la vista en el identificador
+    let DirectorioImgProducto = Marca+Familia+EstructuradosAlias
+    let ImgProducto = DirectorioImgProducto+Color.value
+    ListProductoDescription('OPCAPCC60')
+    ListProductoAdicional('OPCAPCC60')
+    ChangeListImgProducto(DirectorioImgProducto, ImgProducto)
+    
+    showClave(CodigoGenerado)
+    existCodeSapPatchCord(CodigoGenerado)
+    
+    let x = Color.selectedIndex;
+    let y = Color.options;
+    let ColorText = y[x].text    
+
+    let descripcion = "PatchCord Diametro Reducido 28 AWG Cat6 "+Longitud.value+" pie(s) color "+ColorText
+    NombreProductoConfigurable(CodigoGenerado, descripcion)
+    DescPrdConf.innerHTML=descripcion
+    agregarFichaTecnicaConfigurable(Marca+Familia+EstructuradosAlias)
+    agregarCertificadoConfigurable(CodigoGenerado)
+}
+var Categoria6 = function() {
+  switch(Cable.value){
+    case 'PCC6' : 
+      Categoria6UTP()
+    break;
+    case 'PCC6-1' : 
+      Categoria628AWG()
+    break; 
+   
+    default:
+      templateAlert("danger", "", "No se encontro la opción solitada por favor pide ayuda, a tú ejecutivo", "topRight", "icon-slash")
+      console.log("No se encontro la opción solitada por favor pide ayuda, a tú ejecutivo")
+  }
+}
 Categoria6()
