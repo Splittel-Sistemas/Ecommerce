a35676270a8958856b3fc57c1a6e345b84a9824b
aaron0609ac <58867893+aaron0609ac@users.noreply.github.com>
2025-02-28 13:24:00 -0600

    Update Alta.Controller.php
    Email

--- models/Solicitud/Registro/Alta.Controller.php
+++ models/Solicitud/Registro/Alta.Controller.php
@@ -608,7 +608,7 @@ class SolicitudRegistroController
           $host =           'mail.fibremex.com';
           $puerto =    '587';
           $email =  'notificaciones@fibremex.com';
-          $password =   'D6.aWs!F@xwD';
+          $password =   'e!lW.vdLf^_E';
 
           //Este bloque es importante
           $mail = new PHPMailer();
