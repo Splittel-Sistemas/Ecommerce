008684499e4990fd6df2dc33f8b7c4c44e3904fe
Parkz <yosimar.dev@gmail.com>
2026-05-21 18:10:34 -0600

    Fix: se agrega vista de caraga antes de mostrar ficha de pago

--- views/Pedido/Banco/index.php
+++ views/Pedido/Banco/index.php
@@ -1,10 +1,3 @@
-<div class="card text-center" id="cargando-ficha">
-  <div class="card-body padding-top-2x">
-    <p><img src="../../public/images/Otros/loading.gif" width="200px" height="200px" /></p>
-    <h3 class="card-title">¡Cargando!</h3>
-    <p class="card-text">Se esta generando la ficha de pago, por favor espere un momento.</p>
-  </div>
-</div>
 <?php
 if (!class_exists('PedidoController')) {
   include $_SERVER['DOCUMENT_ROOT'] . '/fibra-optica/models/Pedido/Pedido.Controller.php';
@@ -25,11 +18,6 @@ if (count($Pedido->records) > 0) {
 }
 $link = $_SESSION['Ecommerce-ClienteTipo'] == 'B2B' ? '../Cuenta/index.php?menu=4' : '../Cuenta/index.php?menu=4';
 
-?>
-<script>
-  document.getElementById('cargando-ficha').remove();
-</script>
-<?php
 if ($encontrado) {
 ?>
   <div class="card text-center">
--- views/Pedido/Completado.php
+++ views/Pedido/Completado.php
@@ -1,44 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- <title> Contacto </title> -->
-    <?php include $_SERVER["DOCUMENT_ROOT"].'/fibra-optica/views/Partials/Head.php'; ?>
-  </head>
-  <!-- Body-->
-  <body>
-    <!-- Header -->
-    <?php include $_SERVER["DOCUMENT_ROOT"].'/fibra-optica/views/Partials/Header.php'; ?>
-    <!-- Page Title-->
-    <div class="page-title">
-      <div class="container">
-        <div class="column">
-          <h1>Checkout</h1>
-        </div>
-        <div class="column">
-          <ul class="breadcrumbs">
-            <li><a href="../Home/">Home</a>
-            </li>
-            <li class="separator">&nbsp;</li>
-            <li>Checkout</li>
-          </ul>
-        </div>
+
+<head>
+  <!-- <title> Contacto </title> -->
+  <?php include $_SERVER["DOCUMENT_ROOT"] . '/fibra-optica/views/Partials/Head.php'; ?>
+
+  <style>
+    /* Fondo de carga */
+    #cargando-ficha {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: #ffffff5e;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      z-index: 9999;
+    }
+  </style>
+</head>
+<!-- Body-->
+
+<body>
+  <div class="card text-center" id="cargando-ficha">
+    <div class="card-body" style="padding-top: 250px;">
+      <p><img src="../../public/images/Otros/loading.gif" width="200px" height="200px" /></p>
+      <h3 class="card-title">¡Cargando!</h3>
+      <p class="card-text">Se esta generando la ficha de pago, por favor espere un momento.</p>
+    </div>
+  </div>
+
+  <!-- Header -->
+  <?php include $_SERVER["DOCUMENT_ROOT"] . '/fibra-optica/views/Partials/Header.php'; ?>
+  <!-- Page Title-->
+  <div class="page-title">
+    <div class="container">
+      <div class="column">
+        <h1>Checkout</h1>
+      </div>
+      <div class="column">
+        <ul class="breadcrumbs">
+          <li><a href="../Home/">Home</a>
+          </li>
+          <li class="separator">&nbsp;</li>
+          <li>Checkout</li>
+        </ul>
       </div>
     </div>
-    <!-- Page Content-->
-    <div class="container padding-bottom-3x mb-2">
-      <!-- Principal -->
-      <?php 
-        unset($_SESSION["Ecommerce-OpenPay-3DSecure-Id"]);
-        if(isset($_GET['method']) && $_GET['method'] == 'bank'){
-          include $_SERVER['DOCUMENT_ROOT'].'/fibra-optica/views/Pedido/Banco/index.php'; 
-        }else{
-          include $_SERVER['DOCUMENT_ROOT'].'/fibra-optica/views/Pedido/Tarjeta/index.php'; 
-        }
-      ?>
-    </div>
-    <!-- Footer -->
-    <?php include $_SERVER["DOCUMENT_ROOT"].'/fibra-optica/views/Partials/Footer.php'; ?>
-    <!-- scripts JS -->
-    <?php include $_SERVER["DOCUMENT_ROOT"].'/fibra-optica/views/Partials/Scripts.php'; ?>
-  </body>
+  </div>
+  <!-- Page Content-->
+  <div class="container padding-bottom-3x mb-2">
+    <!-- Principal -->
+    <?php
+    unset($_SESSION["Ecommerce-OpenPay-3DSecure-Id"]);
+    if (isset($_GET['method']) && $_GET['method'] == 'bank') {
+      include $_SERVER['DOCUMENT_ROOT'] . '/fibra-optica/views/Pedido/Banco/index.php';
+    } else {
+      include $_SERVER['DOCUMENT_ROOT'] . '/fibra-optica/views/Pedido/Tarjeta/index.php';
+    }
+    ?>
+  </div>
+
+  <script>
+    document.getElementById('cargando-ficha').remove();
+  </script>
+
+  <!-- Footer -->
+  <?php include $_SERVER["DOCUMENT_ROOT"] . '/fibra-optica/views/Partials/Footer.php'; ?>
+  <!-- scripts JS -->
+  <?php include $_SERVER["DOCUMENT_ROOT"] . '/fibra-optica/views/Partials/Scripts.php'; ?>
+</body>
+
 </html>
